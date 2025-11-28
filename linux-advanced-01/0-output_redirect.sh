8bc9371980c211673cb2e56fe65c02b89aa7adff
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-11-28 18:49:37 +0100

    Create 0-output_redirect.sh

---
+++ linux-advanced-01/0-output_redirect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. Print "Hello, World!" to stdout
+echo "Hello, World!"
+
+# 2. Redirect output to file (overwrite)
+echo "First line" > 0-first_output.txt
+
+# 3. Redirect output to file (append)
+echo "Second line" >> 0-first_output.txt
+
+# 4. Display the content of the file to stdout
+cat 0-first_output.txt
